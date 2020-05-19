ce491678749158242ae689860bfb4e174a06cfe6
grey4uk <grey4uk14@gmail.com>
2020-05-19 23:06:33 +0300

    add some style

---
+++ build/precache-manifest.0f6d167cbd222829f2eea2b620145528.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f4d3a353fd6a392f8dbdaa719f65d5",
+    "url": "/rn_animation/index.html"
+  },
+  {
+    "revision": "44711619e77e8f64125c",
+    "url": "/rn_animation/static/css/main.635dee03.chunk.css"
+  },
+  {
+    "revision": "accc48e82ad16357a6d9",
+    "url": "/rn_animation/static/js/2.16bf9035.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rn_animation/static/js/2.16bf9035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44711619e77e8f64125c",
+    "url": "/rn_animation/static/js/main.5eb9e5fe.chunk.js"
+  },
+  {
+    "revision": "b2f66f9c5e4349c8603c",
+    "url": "/rn_animation/static/js/runtime-main.cf51d33e.js"
+  },
+  {
+    "revision": "93bbc8495b87c4615f3fa146faf45d75",
+    "url": "/rn_animation/static/media/anime.93bbc849.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useEffect } from "react";
 import css from "./App.module.css";
 
 const App = () => {
-  let intervalID=null;
-  
+  let intervalID = null;
+
   let xx = 0;
   let yy = 0;
 
   const handleFront = (e) => {
     clearInterval(intervalID);
     const animate = document.getElementById("hero");
-    intervalID=setInterval(() => {
+    intervalID = setInterval(() => {
       animate.style.backgroundPositionX = xx + "px";
       animate.style.backgroundPositionY = yy + "px";
       xx -= animate.clientWidth;
@@ -25,25 +25,25 @@ const App = () => {
   const handleBack = (e) => {
     clearInterval(intervalID);
     const animate = document.getElementById("hero");
-    intervalID=setInterval(() => {
+    intervalID = setInterval(() => {
       animate.style.backgroundPositionX = xx + "px";
       animate.style.backgroundPositionY = yy + "px";
       xx -= animate.clientWidth;
-      yy = -3 * animate.clientHeight-6;
+      yy = -3 * animate.clientHeight - 6;
       if (xx < -2 * animate.clientWidth) {
         xx = 0;
-      } 
+      }
     }, 200);
   };
 
   const handleLeft = (e) => {
     clearInterval(intervalID);
     const animate = document.getElementById("hero");
-    intervalID=setInterval(() => {
+    intervalID = setInterval(() => {
       animate.style.backgroundPositionX = xx + "px";
       animate.style.backgroundPositionY = yy + "px";
       xx -= animate.clientWidth;
-      yy = -animate.clientHeight-4;
+      yy = -animate.clientHeight - 4;
       if (xx < -2 * animate.clientWidth) {
         xx = 0;
       }
@@ -53,11 +53,11 @@ const App = () => {
   const handleRight = (e) => {
     clearInterval(intervalID);
     const animate = document.getElementById("hero");
-    intervalID=setInterval(() => {
+    intervalID = setInterval(() => {
       animate.style.backgroundPositionX = xx + "px";
       animate.style.backgroundPositionY = yy + "px";
       xx -= animate.clientWidth;
-      yy = -2*animate.clientHeight-4;
+      yy = -2 * animate.clientHeight - 4;
       if (xx < -2 * animate.clientWidth) {
         xx = 0;
       }
@@ -65,14 +65,14 @@ const App = () => {
   };
 
   return (
-    <div>
-      <div id="hero" className={css.animate}></div>
+    <div className={css.container}>
+      <button onClick={handleBack}>Назад</button>
       <div className={css.buttonsBlock}>
-        <button onClick={handleFront}>Вперед</button>
-        <button onClick={handleBack}>Назад</button>
         <button onClick={handleLeft}>Вліво</button>
+        <div id="hero" className={css.animate}></div>
         <button onClick={handleRight}>Вправо</button>
       </div>
+      <button onClick={handleFront}>Вперед</button>
     </div>
   );
 };
