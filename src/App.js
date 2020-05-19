d3938b061daf093aac8b785851775e73cf7b3851
grey4uk <grey4uk14@gmail.com>
2020-05-19 22:12:29 +0300

    init prob of animation

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from "react";
+import css from "./App.module.css";
+
+const App = () => {
+  let intervalID=null;
+  
+  let xx = 0;
+  let yy = 0;
+
+  const handleFront = (e) => {
+    clearInterval(intervalID);
+    const animate = document.getElementById("hero");
+    intervalID=setInterval(() => {
+      animate.style.backgroundPositionX = xx + "px";
+      animate.style.backgroundPositionY = yy + "px";
+      xx -= animate.clientWidth;
+      if (xx < -2 * animate.clientWidth) {
+        xx = 0;
+      } else {
+        yy = 0;
+      }
+    }, 200);
+  };
+
+  const handleBack = (e) => {
+    clearInterval(intervalID);
+    const animate = document.getElementById("hero");
+    intervalID=setInterval(() => {
+      animate.style.backgroundPositionX = xx + "px";
+      animate.style.backgroundPositionY = yy + "px";
+      xx -= animate.clientWidth;
+      yy = -3 * animate.clientHeight-6;
+      if (xx < -2 * animate.clientWidth) {
+        xx = 0;
+      } 
+    }, 200);
+  };
+
+  const handleLeft = (e) => {
+    clearInterval(intervalID);
+    const animate = document.getElementById("hero");
+    intervalID=setInterval(() => {
+      animate.style.backgroundPositionX = xx + "px";
+      animate.style.backgroundPositionY = yy + "px";
+      xx -= animate.clientWidth;
+      yy = -animate.clientHeight-4;
+      if (xx < -2 * animate.clientWidth) {
+        xx = 0;
+      }
+    }, 200);
+  };
+
+  const handleRight = (e) => {
+    clearInterval(intervalID);
+    const animate = document.getElementById("hero");
+    intervalID=setInterval(() => {
+      animate.style.backgroundPositionX = xx + "px";
+      animate.style.backgroundPositionY = yy + "px";
+      xx -= animate.clientWidth;
+      yy = -2*animate.clientHeight-4;
+      if (xx < -2 * animate.clientWidth) {
+        xx = 0;
+      }
+    }, 200);
+  };
+
+  return (
+    <div>
+      <div id="hero" className={css.animate}></div>
+      <div className={css.buttonsBlock}>
+        <button onClick={handleFront}>Вперед</button>
+        <button onClick={handleBack}>Назад</button>
+        <button onClick={handleLeft}>Вліво</button>
+        <button onClick={handleRight}>Вправо</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
